ec36e4d9987dee60fc19d3ff7e9f3d35d0d7b224
Chris Haid <chaid@kippchicago.org>
2016-06-22 11:55:05 -0500

    created utils and added calc_academic_year() function and documentation.

---
+++ R/utils.R
@@ -0,0 +1,36 @@
+#' Calculate School or Academic Year from date
+#'
+#' @param x the date to be parsed
+#' @param format one of `long`, `short`, `first_year`, or `second_year`,
+#' which are rendered as `SY 2015-2016`, `15-16`, `2015`, or `2016`, respectively,
+#' for the 2015-2016 school year.
+#' @param date_parser a parser (such as as \code{lubrdiate}'s \code{\link[lubridate]{ymd}} function)
+#'
+#' @return a character or integer vector
+#' @export
+#'
+#' @examples
+#' calc_academic_year('2014-09-15')
+calc_academic_year <- function(x,
+                               format = "long",
+                               date_parser=lubridate::ymd) {
+
+  date_parsed <- date_parser(x) %>% as.POSIXlt()
+  out <- date_parsed$year + (date_parsed$mo >= 6) + 1899
+
+  # return SY
+  if(format == "long") out <- sprintf("SY %s-%s", out, out+1)
+
+  if(format == "short") {
+    out <- sprintf("%s-%s",
+                   stringr::str_extract(out, "\\d{2}$"),
+                   stringr::str_extract(out+1, "\\d{2}$"))
+  }
+
+  if(format == "first_year") out <- out
+  if(format == "second_year") out <- out + 1
+
+
+  out
+
+}
