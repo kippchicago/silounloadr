6d20be7de2d1df3110f6cb5da89c85989c272188
Chris Haid <chaid@kippchicago.org>
2016-06-27 15:45:22 -0500

    Created calc_ps_termid function.

--- R/utils.R
+++ R/utils.R
@@ -34,3 +34,20 @@ calc_academic_year <- function(x,
   out
 
 }
+
+
+#' Calculate PowerSchool TermId given first year of a school year
+#'
+#' @param sy First year in a school year (e.g. 2015 for SY 2015-2016)
+#' @param quarter the quarter as integer in a year. The default is 0 for
+#' the school year's termid.
+#'
+#' @return a character or integer vector
+#' @export
+#'
+#' @examples
+#' calc_ps_termid(2015)
+calc_ps_termid <- function(sy,
+                           quarter = 0) {
+  (sy - 1990)*100 + quarter
+}
