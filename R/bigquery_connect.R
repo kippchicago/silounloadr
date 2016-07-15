ce5645540cab08e9dab6536c373a70b78409be98
Chris Haid <chaid@kippchicago.org>
2016-07-15 16:54:05 -0500

    Add BigQuery connectivity
    Closes #1 and #2

---
+++ R/bigquery_connect.R
@@ -0,0 +1,36 @@
+#' Create connection to BigQuery warehouse and return object to global environment
+#'
+#' @param dataset name in BigQuery project  which will be connected to.
+#'
+#' @description This makes a connection to BigQuery via `bigrquery`.  This call requires
+#' that system environment variable for BigQuery are set (typically with a an `.Renviron`
+#' file in the users home directory that is sourced when R is started).  This
+#' function is called for it's side effect: createing a connection object for
+#' the named database that can be used by `dplyr` calls in other functions in
+#' this package.
+#'
+#' @return a src object
+#' @export
+#'
+#' @examples
+#'
+#' connect_to_bq('PARCC')
+#'
+connect_to_bq <- function(dataset_name) {
+
+  dataset_title <- sprintf("bq_%s",
+                     toupper(dataset_name))
+
+
+  project_name <- Sys.getenv("BQ_PROJECT")
+
+  connection_name <- tolower(dataset_title)
+
+  conn <- bigrquery::src_bigquery(project = project_name,
+                                  dataset = dataset_name
+                                  )
+
+  # Using a call to global so that this connection object is only made once
+  # and is available for all get_* functions
+  do.call("<<-", list(connection_name, conn))
+}
---
+++ R/parcc.R
@@ -0,0 +1,35 @@
+#' Collect table from PARCC
+#'
+#' @param table_name name of a table from PARCC dataset
+#' @param collect whether or not table should be pulled into local R session.
+#' Default is `FALSE`.
+#'
+#' @return a data.frame
+#' @export
+#'
+
+get_parcc <- function(table_name = 'parcc_student_results',
+                      collect = FALSE){
+
+  #check if bq_parcc exists; if not create it
+
+
+  if (!exists("bq_parcc")) {
+    connect_to_bq("PARCC")
+  } else {
+    if (!bigrquery::dbIsValid(bq_parcc$con)) {
+      connect_to_bq("PARCC")
+    }
+  }
+
+
+  out <- dplyr::tbl(bq_parcc, table_name)
+
+  if (collect) {
+    out <- dplyr::collect(out)
+    names(out) <- tolower(names(out))
+  }
+
+  out
+
+}
